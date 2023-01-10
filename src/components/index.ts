7185a57ceec7ff5b7c6bb443b41cbc38c037d5a1
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-01-10 16:20:58 -0500

    testing for Garner

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './Home'
+export * from './Navbar';
+export * from './Inventory';
+export * from './Car';
+export * from './About';
+export * from './SignIn';
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQrja2QR9KEgebvX9jfXqCyapzXpmWzT0",
+  authDomain: "car-inventory2.firebaseapp.com",
+  projectId: "car-inventory2",
+  storageBucket: "car-inventory2.appspot.com",
+  messagingSenderId: "612342713235",
+  appId: "1:612342713235:web:f9279dbb7dd46ddb743666"
+}; 
